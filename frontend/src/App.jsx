5be685313e311a48e37d56afeceae20b3636d540
fiza noor <fnoor034726@gmail.com>
2026-02-16 20:16:42 +0500

    UI Changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 
+import { Button } from "./components/ui/button";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <>
     <Dashboard />
+    <Button variant="outline" className="mt-4">
+      Click me
+    </Button>
     </>
   );
 }
